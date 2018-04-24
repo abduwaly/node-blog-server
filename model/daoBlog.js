a45bbd27db102eae072af425302881468ea99ecc
arsay <arsay@foxmail.com>
2018-04-24 12:08:53 +0800

    fix: code style;

--- model/daoBlog.js
+++ model/daoBlog.js
@@ -1,16 +1,15 @@
 var dbUtil = require("../utils/dbUtil");
 
-function Blog(){
-	
-	this.list = function(onSuccess,onErr){
-		var sql = 'SELECT * FROM blog';
-		dbUtil.exeSql(sql,onSuccess,onErr);
-	},
-	
-	this.getById = function(id,onSuccess,onErr){
-		var sql = 'Select * from blog where id='+id;
-		dbUtil.exeSql(sql,onSuccess,onErr);
-	}
-	
+function Blog() {
+  
+  this.list = function(onSuccess, onErr) {
+    var sql = 'SELECT * FROM blog';
+    dbUtil.exeSql(sql, onSuccess, onErr);
+  };
+  
+  this.getById = function(id, onSuccess, onErr) {
+    var sql = 'Select * from blog where id=' + id;
+    dbUtil.exeSql(sql, onSuccess, onErr);
+  }
 }
 module.exports = Blog;

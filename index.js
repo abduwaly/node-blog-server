a29669dca23dd4b6cac0b5f22b57b1d5dc169640
arsay <arsay@foxmail.com>
2018-04-25 16:03:25 +0800

    add : CORS setup;

--- index.js
+++ index.js
@@ -13,6 +13,20 @@ var server = app.listen(3000, function () {
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+// CORS 设置
+app.all('*',function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+
+  if (req.method == 'OPTIONS') {
+    res.send(200); /让options请求快速返回/
+  }
+  else {
+    next();
+  }
+});
+
 // Manager
 app.use('/manager', manager);
 
